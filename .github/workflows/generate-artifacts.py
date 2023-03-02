291192229ce6cf7a47415b3bae8738d9eff8c6fe
allabakashb <allabakash0@gmail.com>
2023-03-02 10:31:07 +0530

    Update generate-artifacts.py

--- .github/workflows/generate-artifacts.py
+++ .github/workflows/generate-artifacts.py
@@ -5,20 +5,20 @@ on:
       - develop
 
 jobs:
-  comment:
+  artifacts:
     runs-on: ubuntu-latest
     steps:
-    - uses: actions/checkout@v3
+      - uses: actions/checkout@v3
 
-    - run: mkdir -p path/to/artifact
+      - run: mkdir -p path/to/artifact
 
-    - run: echo hello1 > path/to/artifact/test_file1.txt
-    - run: echo hello2 > path/to/artifact/test_file2.txt
+      - run: echo hello1 > path/to/artifact/test_file1.txt
+      - run: echo hello2 > path/to/artifact/test_file2.txt
 
-    - name: Tar files
-      run: tar -cvf testcases.tar path/to/artifact
+      - name: Tar files
+        run: tar -cvf testcases.tar path/to/artifact
 
-    - uses: actions/upload-artifact@v3
-      with:
-        name: testcases-artifact
-        path: testcases.tar
+      - uses: actions/upload-artifact@v3
+        with:
+          name: testcases-artifact
+          path: testcases.tar
