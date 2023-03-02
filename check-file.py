c0f2b7249d7583b5677b8d6ea91549bb7ba2ff83
Brajesh Jaishwal <brajesh.jaishwal@gmail.com>
2023-03-02 14:49:45 +0530

    Update check-file.py

--- check-file.py
+++ check-file.py
@@ -4,3 +4,21 @@ def fun_func(ip: str) -> str:
     return 'output'
   else:
     return 'something'
+ 
+def define_fun(time: str) -> str:
+  #celebrate this
+  if time == 'morning':
+    return 'having morning tea'
+  elif time == 'afternoon':
+    return 'black cofee'
+  else:
+    return 'long walk and workout session'
+ 
+class FunDefHandler():
+    def __init__(self, conn_str: str):
+      #define connection object
+      self.conn = None
+    
+    def get_conn():
+      return self.conn
+  
