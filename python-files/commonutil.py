ea6c3d45b31ae3f1df85b3cad4423facfad81a95
Brajesh Jaishwal <brajesh.jaishwal@gmail.com>
2023-03-03 18:50:08 +0530

    Delete python-files directory

--- python-files/commonutil.py
+++
@@ -1,6 +0,0 @@
-class CommonUtils:
-    def generate_binds_question_mark(self, binds: list) -> str:
-        if binds is None:
-            raise Exception(
-                'binds cannot be null to generate_binds_question_mark')
-        return ','.join(['?' for i in range(len(binds))])
