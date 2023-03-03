5c2a0e7ef37466f365437630d41fa2ed10531632
allabakashb <allabakash0@gmail.com>
2023-03-03 17:27:55 +0530

    Update generate_testcase.py

--- generate_testcase.py
+++ generate_testcase.py
@@ -6,27 +6,47 @@ import os
 import requests
 import glob
 import os
+from github import Github
 
 # Authenticating with the OpenAI API
 openai.api_key = os.getenv('OPENAPI_KEY')
 openai_engine = 'text-davinci-003'
-openai_temperature = 1
-openai_max_tokens = 4000
+openai_temperature = 0.5
+openai_max_tokens = 2048
 
+g = Github(os.getenv('GIT_TOKEN'))
 
 def generate_testcases():
     try:
-        fl_list = glob.glob('python-files/*.py')
-        print('file list ', fl_list)
-
+        repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'))
+        pull_request = repo.get_pull(int(os.getenv('GIT_PR_ID')))
+        
         if not os.path.exists('test-genie'):
             os.makedirs('test-genie')
-
-        for file in fl_list:
-            with open(file) as f:
-                content = f.read()
+ 
+        ## Loop through the commits in the pull request
+        commits = pull_request.get_commits()
+        
+        seen_files = set()
+        for commit in commits:
+            files = commit.files
+            
+            print('file list ', files)
+            for file in files:
+                filename = file.filename
                 
-                # Sending the code to ChatGPT
+                if filename in seen_files:
+                    print(f'Unit tests already generated for file {filename}')
+                    continue
+
+                if 'python-files' not in filename:
+                    print(f'{filename } Not a python file. skipping ')
+                    continue
+
+                seen_files.add(filename)
+                content = repo.get_contents(filename, ref=commit.sha).decoded_content
+ 
+                # Sending the code to ChatGPT from here
                 response = openai.Completion.create(
                     engine=openai_engine,
                     prompt=(
@@ -34,15 +54,15 @@ def generate_testcases():
                     temperature=openai_temperature,
                     max_tokens=openai_max_tokens
                 )
-                filename = f.name.split("/")[1]
                 
                 print(f'test cases generated for "{filename}": \n',  {
                     response['choices'][0]['text']})
-                    
-                with open(f"test-genie/{filename}", "w") as ws:
+                
+                test_file_name = 'test_' + filename.split('/')[-1]
+                with open(f"test-genie/{test_file_name}", "w") as ws:
                     ws.write(response['choices'][0]['text'])
 
-                with open(f"test-genie/{filename}") as rs:
+                with open(f"test-genie/{test_file_name}") as rs:
                     content = rs.read()
                     print(f"test cases read from the files \n, {content}")
     except Exception as ex:
