d8b03c612ed24e6429fc7a9e9628b0db822c442a
allabakashb <allabakash0@gmail.com>
2023-03-03 18:45:15 +0530

    Create download.tsx

---
+++ src/download.tsx
@@ -0,0 +1,11 @@
+export const downloadFile = async (props: DownloadProps) => {
+    const blob = new Blob([props.content.toString()], { type: props.type });
+    const url = window.URL.createObjectURL(blob);
+    const link1 = document.createElement(aTag);
+    const link = link1;
+    link.href = url;
+    link.setAttribute(downloadAttr, props.fileName);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
